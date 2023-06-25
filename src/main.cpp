8d9e4f4c63cf26ec29344f69dc88fe2fccf2816a
Christian Bouwense <bouwensec@gmail.com>
2023-06-25 12:25:24 -0400

    set speed as variable

--- src/main.cpp
+++ src/main.cpp
@@ -3,58 +3,62 @@
 #include "handleKeyboardEvents.h"
 
 void moveBasedOnMovementState(sf::RectangleShape& rectangle, const MovementState& movementState) {
-  if (movementState.isMovingRight) {
-    rectangle.move(0.1f, 0.f);
-  }
+const float speed = 0.1f;
 
-  if (movementState.isMovingLeft) {
-    rectangle.move(-0.1f, 0.f);
-  }
+if (movementState.isMovingRight) {
+  rectangle.move(speed, 0.f);
+}
 
-  if (movementState.isMovingUp) {
-    rectangle.move(0.f, -0.1f);
-  }
+if (movementState.isMovingLeft) {
+  rectangle.move(-speed, 0.f);
+}
 
-  if (movementState.isMovingDown) {
-    rectangle.move(0.f, 0.1f);
-  }
+if (movementState.isMovingUp) {
+  rectangle.move(0.f, -speed);
+}
+
+if (movementState.isMovingDown) {
+  rectangle.move(0.f, speed);
+}
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1024, 1024), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(1024, 1024), "SFML works!");
 
-    sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
-    rectangle.setFillColor(sf::Color::Red);
-    rectangle.setPosition(100.f, 100.f);
+  sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
+  rectangle.setFillColor(sf::Color::Red);
+  rectangle.setPosition(100.f, 100.f);
 
-    MovementState movementState = {
-      false,
-      false,
-      false,
-      false
-    };
+  MovementState movementState = {
+    false,
+    false,
+    false,
+    false
+  };
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-              window.close();
-            }
+  
 
-            if (event.key.code == sf::Keyboard::Escape) {
-              window.close();
-            }
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
 
-            movementState = newStateOnKeyPressed(event, movementState);
-            movementState = newStateOnKeyReleased(event, movementState);
-        }
+      if (event.key.code == sf::Keyboard::Escape) {
+        window.close();
+      }
 
-        moveBasedOnMovementState(rectangle, movementState);
-
-        window.clear();
-        window.draw(rectangle);
-        window.display();
+      movementState = newStateOnKeyPressed(event, movementState);
+      movementState = newStateOnKeyReleased(event, movementState);
     }
 
-    return 0;
+    moveBasedOnMovementState(rectangle, movementState);
+
+    window.clear();
+    window.draw(rectangle);
+    window.display();
+  }
+
+  return 0;
 }
